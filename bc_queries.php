def29d19d7951b8b0afec043706819ef3fda4e7f
Adam Ploof <adamploof@hotmail.com>
2020-03-18 00:24:27 -0400

    loading the drill-down table now sends requests concurrently

--- bc_queries.php
+++ bc_queries.php
@@ -2,7 +2,9 @@
 // Various queries for retrieving bandcamp data
 
 $query_set = array(
-    "select_all" => "SELECT * FROM `notables_tab` ORDER BY `date`",
+    "select_all" => "SELECT * FROM `notables_tab`
+        WHERE `date` BETWEEN :start_date AND :end_date
+        ORDER BY `date`",
 
     "sum_by_date" => "SELECT `date`, SUM(amount) AS 'total', COUNT(id) AS 'count'
         FROM `notables_tab`
--- static/js/bc_drill_down.js
+++ static/js/bc_drill_down.js
@@ -28,6 +28,7 @@ function mouseDown(e) {
         window.removeEventListener("mouseup", mouseUp);
 
         // make sure the user hasn't dragged the window out of bounds
+        // if so, move the window back in bounds
         if (trackWindow.offsetHeight <= 27) {
             trackWindow.style.height = "25px";
             genreWindow.style.height = "462px";
@@ -40,7 +41,6 @@ function mouseDown(e) {
 
 // Send ajax request to genre_table.php to repopulate table when user
 // selects a date to drill down into
-
 const dateItems = document.getElementsByClassName("list-group-item list-group-item-action");
 
 for (let i = 0; i < dateItems.length; i++) {
@@ -49,17 +49,46 @@ for (let i = 0; i < dateItems.length; i++) {
 
 function fetchTableInfo(e) {
     e.preventDefault();
-    let fetch_date = this.dataset.date;
-    let genre_q = "genre";
-    let genre_id = "genre-body";
-    let tracks_q = "tracks";
-    let tracks_id= "tracks-body";
-
-    loadGenres(fetch_date, genre_q, genre_id);
-    // loadTracks(fetch_date, tracks_q, tracks_id);
+    date = this.dataset.date
+    initTables(date);
 }
 
-function loadGenres(fetch_date, tbl_q, tbl_id) {
+
+function initTables(date) {
+    // Handles requests to bc_ajax.php and chains together promises to load them concurrently
+    const tbl_obj = {
+        fetch_date: date,
+        genre_q: "genre",
+        tracks_q: "tracks",
+    }
+
+    loadTable(tbl_obj.fetch_date, tbl_obj.genre_q)
+        .then(function(data) {
+            let tbl = document.getElementById("genre-body");
+            clearTable(tbl);
+            tbl.innerHTML = data;
+        }).then(function() {
+            loadTable(tbl_obj.fetch_date, tbl_obj.tracks_q)
+                .then(function(data) {
+                    let tbl = document.getElementById("tracks-body");
+                    clearTable(tbl);
+                    tbl.innerHTML = data;
+                })
+        }).catch(function(e) {
+            console.log(e);
+        })
+
+}
+
+function clearTable(tbl) {
+    // Clear the table of old data before refreshing with new
+    while (tbl.firstChild) {
+        tbl.firstChild.remove();
+    }
+}
+
+
+function loadTable(fetch_date, tbl_q) {
 
     // Constructing the URL
     let url = new URL("http://localhost:8080/bc_notables/bc_ajax.php");
@@ -72,64 +101,20 @@ function loadGenres(fetch_date, tbl_q, tbl_id) {
 
     // Create the request
     request = new XMLHttpRequest();
-    request.responseType = 'text';
-    request.open('GET', url.toString(), true);
+
+    return new Promise(function(resolve, reject) {
+        // returns the response on success
+        request.onload = function() {
+            let data = request.response;
+            if (this.status == 200) {
+                resolve(data);
+            } else {
+                reject("Could not retrieve data");
+            }
+        }
     
-    request.onload = function() {
-        let tbl = document.getElementById(tbl_id);
-        let data = request.response;
-        if (this.status == 200) {
-            clearTable(tbl);
-            tbl.innerHTML = data;
-        } else {
-            console.log("could not retrieve data");
-        }
-    }
-
-    function clearTable(tbl) {
-        // Clear the table of old data before refreshing with new
-        while (tbl.firstChild) {
-            tbl.firstChild.remove();
-        }
-    }
-
-    request.send();
-}
-
-// TODO: Combine the fetch functions into a single fetchTable function
-function loadTracks(fetch_date, tbl_q, tbl_id) {
-
-    // Constructing the URL
-    let url = new URL("http://localhost:8080/bc_notables/bc_ajax.php");
-    let params = new URLSearchParams();
-
-    params.append('start_date', fetch_date);
-    params.append('end_date', fetch_date);
-    params.append('tbl', tbl_q);
-    url.search = params.toString();
-
-    // Create the request
-    request = new XMLHttpRequest();
-    request.responseType = 'text';
-    request.open('GET', url.toString(), true);
-    
-    request.onload = function() {
-        let tbl = document.getElementById(tbl_id);
-        let data = request.response;
-        if (this.status == 200) {
-            clearTable(tbl);
-            tbl.innerHTML = data;
-        } else {
-            console.log("could not retrieve data");
-        }
-    }
-
-    function clearTable(tbl) {
-        // Clear the table of old data before refreshing with new
-        while (tbl.firstChild) {
-            tbl.firstChild.remove();
-        }
-    }
-
-    request.send();
+        request.responseType = 'text';
+        request.open('GET', url.toString(), true);
+        request.send();
+    });
 }
